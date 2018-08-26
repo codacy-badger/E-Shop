acb2d26d1113b56d3157f720e44b3f30e3a70cd4
Abderrahim <abderrahim.m.elbaz@gmail.com>
2018-08-26 10:15:30 +0200

    [E-SHOP] [ configuration - firebase]

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: 'AIzaSyC_Fc6Ov7RbHHdMO2mvjpaHzArjjjguiqs',
+    authDomain: 'shop-1b2eb.firebaseapp.com',
+    databaseURL: 'https://shop-1b2eb.firebaseio.com',
+    projectId: 'shop-1b2eb',
+    storageBucket: 'shop-1b2eb.appspot.com',
+    messagingSenderId: '833736646331'
+  }
 };
