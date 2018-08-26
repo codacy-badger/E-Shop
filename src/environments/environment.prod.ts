772ce2793ef42ba8b8831fe8f697cdb6b3640a11
Abderrahim <abderrahim.m.elbaz@gmail.com>
2018-08-26 10:19:42 +0200

    [E-SHOP][config firebase prod]

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyC_Fc6Ov7RbHHdMO2mvjpaHzArjjjguiqs',
+    authDomain: 'shop-1b2eb.firebaseapp.com',
+    databaseURL: 'https://shop-1b2eb.firebaseio.com',
+    projectId: 'shop-1b2eb',
+    storageBucket: 'shop-1b2eb.appspot.com',
+    messagingSenderId: '833736646331'
+  }
 };
