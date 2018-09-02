2e4dbe3360c9a0efe927370ebae308a4460d71b6
Abderrahim <abderrahim.m.elbaz@gmail.com>
2018-09-02 23:25:19 +0200

    [Eshop : Manage products -  use  & configure and fill ng datatble]

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,6 +1,8 @@
+import { Product } from './../../models/product';
 import { ProductService } from './../../product.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from '../../../../node_modules/rxjs/Subscription';
+import { DataTableResource } from '../../../../node_modules/angular-4-data-table';
 
 @Component({
   selector: 'app-admin-products',
@@ -9,12 +11,32 @@ import { Subscription } from '../../../../node_modules/rxjs/Subscription';
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
 
-  products: { title: string }[];
+  products: Product[];
   filtredProducts: any[];
   subscription: Subscription;
+  tableRessource: DataTableResource<Product>;
+  items: Product[] = [];
+  itemCount: number;
+
   constructor(private productService: ProductService) {
     this.subscription = this.productService.getAll()
-      .subscribe(products => this.filtredProducts = this.products = products);
+      .subscribe(products => {
+        this.filtredProducts = this.products = products;
+        this.initializeTable(products);
+      });
+  }
+
+  private initializeTable(products: Product[]) {
+    this.tableRessource = new DataTableResource(products);
+    this.tableRessource.query({ offset: 0 })
+      .then(items => this.items = items);
+    this.tableRessource.count().then(count => this.itemCount = count);
+  }
+
+  reloadItems(params) {
+    if (!this.tableRessource) { return; }
+    this.tableRessource.query(params)
+      .then(items => this.items = items);
   }
 
   ngOnInit() {
@@ -26,6 +48,7 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   filter(query: string) {
     this.filtredProducts = (query) ? this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
       this.products;
+      this.initializeTable( this.filtredProducts);
   }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule} from '@angular/forms';
 import { CustomFormsModule } from 'ng2-validation';
+import { DataTableModule } from 'angular-4-data-table';
 
 import { AppComponent } from './app.component';
 import { environment } from 'environments/environment';
@@ -47,6 +48,7 @@ import { ProductFormComponent } from './admin/product-form/product-form.componen
     BrowserModule,
     FormsModule,
     CustomFormsModule,
+    DataTableModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
